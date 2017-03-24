4728c73a2b2324f149824d210a78bcf04091cc41
Stéphane Caron <stephane.caron@normalesup.org>
2017-03-24 16:11:35 +0100

    :lipstick:

--- full_support_area/full_support_area.py
+++ full_support_area/full_support_area.py
@@ -3,20 +3,21 @@
 #
 # Copyright (C) 2015-2016 Stephane Caron <stephane.caron@normalesup.org>
 #
-# This file is part of tro-2016 <https://github.com/stephane-caron/tro-2016>.
+# This file is part of multi-contact-zmp
+# <https://github.com/stephane-caron/multi-contact-zmp>.
 #
-# tro-2016 is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+# multi-contact-zmp is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
-# tro-2016 is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# multi-contact-zmp is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU General Public License along with
-# tro-2016. If not, see <http://www.gnu.org/licenses/>.
+# multi-contact-zmp. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
